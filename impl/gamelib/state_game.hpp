7930970e6cf71e589adb5ac5298271db651f0d93
Splingo <sebastianlillge@gmail.com>
2025-01-25 20:23:02 +0100

    added powerups to test_level and removed duplicate test_level

--- impl/gamelib/state_game.hpp
+++ impl/gamelib/state_game.hpp
@@ -16,7 +16,7 @@
 
 class StateGame : public jt::GameState {
 public:
-    explicit StateGame(std::string const& levelName = "bubble_test_level_pu.json");
+    explicit StateGame(std::string const& levelName = "bubble_test_level.json");
 
 private:
     std::shared_ptr<jt::Box2DWorldInterface> m_world { nullptr };
